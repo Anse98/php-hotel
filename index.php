715ed0e04fbaf450b759629010cd40565e06c9a8
Anse98 <lorenzo.anselmi98.com>
2023-11-22 18:32:14 +0100

    Messi i dati in una sola tabella

--- index.php
+++ index.php
@@ -39,6 +39,7 @@ $hotels = [
   ],
 
 ];
+
 ?>
 
 
@@ -50,40 +51,35 @@ $hotels = [
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>PHP Hotel</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  <link rel="stylesheet" href="./style.css">
 </head>
 
 <body class="d-flex flex-column align-items-center">
 
-  <div class="title text-center">
+  <div class="title text-center mb-4">
     <h1>Hotels</h1>
   </div>
 
-  <div class="p-3 w-50">
+  <div class="px-2 border w-75 table">
 
     <?php
 
     foreach ($hotels as $key => $hotel) { ?>
-
-      <div class="my-5 border">
+      <div class="d-flex border-bottom">
         <?php
-
         foreach ($hotels[$key] as $scndKey => $value) {
         ?>
 
-          <div class="d-flex justify-content-between align-items-center border-bottom p-3">
-            <p class="m-0"><b>
-                <?php echo $scndKey . ' :' ?>
-              </b></p>
-            <p class="m-0">
-              <?php echo $value ?>
-            </p>
+          <div class="block">
+            <h5 class="border-bottom p-0 mb-0 py-2"><?php echo $scndKey ?></h5>
+            <span class="hotels p-0 lh-lg"><?php echo $value ?></span>
           </div>
 
         <?php
-
         }
         ?>
       </div>
+
     <?php
 
     }
