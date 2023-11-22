f1d1fc96465ae94c4d4d9d2decbf8603ff2c380b
Anse98 <lorenzo.anselmi98.com>
2023-11-22 16:41:14 +0100

    Stampati dati in una tabella con bootstrap

--- index.php
+++ index.php
@@ -39,11 +39,62 @@ $hotels = [
   ],
 
 ];
-
-foreach ($hotels as $key => $hotel) {
-  foreach ($hotels[$key] as $scndKey => $value) { ?>
-    <p><?php echo $scndKey . ': ' . $value; ?></p>
-<?php
-  }
-}
 ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP Hotel</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+
+<body class="d-flex flex-column align-items-center">
+
+  <div class="title text-center">
+    <h1>Hotels</h1>
+  </div>
+
+  <div class="p-3 w-50">
+
+    <?php
+
+    foreach ($hotels as $key => $hotel) { ?>
+
+      <div class="my-5 border">
+        <?php
+
+        foreach ($hotels[$key] as $scndKey => $value) {
+        ?>
+
+          <div class="d-flex justify-content-between align-items-center border-bottom p-3">
+            <p class="m-0"><b>
+                <?php echo $scndKey . ' :' ?>
+              </b></p>
+            <p class="m-0">
+              <?php echo $value ?>
+            </p>
+          </div>
+
+        <?php
+
+        }
+        ?>
+      </div>
+    <?php
+
+    }
+
+    ?>
+
+  </div>
+
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</body>
+</body>
+
+</html>
